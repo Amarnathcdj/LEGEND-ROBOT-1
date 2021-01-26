672a26c7e35d5ff7042c2e92943105e1f80402b5
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-01-26 12:02:02 +0530

    Update rmbg.py

--- anie/modules/rmbg.py
+++ anie/modules/rmbg.py
@@ -1,4 +1,5 @@
-from anie import update
+
+from anie import tbot
 from anie import MONGO_DB_URI
 from pymongo import MongoClient
 import io
@@ -10,7 +11,7 @@ from telethon import types
 from telethon.tl import functions
 from anie import REM_BG_API_KEY
 from anie import TEMP_DOWNLOAD_DIRECTORY
-from anie.event import register
+from anie.events import register
 
 
 async def is_register_admin(chat, user):
@@ -18,15 +19,15 @@ async def is_register_admin(chat, user):
 
         return isinstance(
             (
-                await update(functions.channels.GetParticipantRequest(chat, user))
+                await tbot(functions.channels.GetParticipantRequest(chat, user))
             ).participant,
             (types.ChannelParticipantAdmin, types.ChannelParticipantCreator),
         )
     if isinstance(chat, types.InputPeerChat):
 
-        ui = await update.get_peer_id(user)
+        ui = await tbot.get_peer_id(user)
         ps = (
-            await update(functions.messages.GetFullChatRequest(chat.chat_id))
+            await tbot(functions.messages.GetFullChatRequest(chat.chat_id))
         ).full_chat.participants.participants
         return isinstance(
             next((p for p in ps if p.user_id == ui), None),
@@ -67,7 +68,7 @@ async def _(event):
         reply_message = await event.get_reply_message()
         await event.reply("Processing...")
         try:
-            downloaded_file_name = await update.download_media(
+            downloaded_file_name = await tbot.download_media(
                 reply_message, TEMP_DOWNLOAD_DIRECTORY
             )
         except Exception as e:
@@ -83,7 +84,7 @@ async def _(event):
     if "image" in contentType:
         with io.BytesIO(output_file_name.content) as remove_bg_image:
             remove_bg_image.name = "rmbg.png"
-            await update.send_file(
+            await tbot.send_file(
                 event.chat_id,
                 remove_bg_image,
                 force_document=True,
